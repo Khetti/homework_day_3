9f34cb1997f9e60690f9cdfd0733a5d60bea920e
Greg Shanks <greg.shanks@gmail.com>
2019-11-06 22:04:05 +0000

    completed and added exercise_b_users.rb

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -38,6 +38,10 @@ users = {
     {
       :name => "rupert",
       :species => "parrot"
+    },
+    {
+      :name => "fluffy",
+      :species => "dog"
     }
   ]
   },
@@ -51,16 +55,57 @@ users = {
         :species => "snake"
       }
     ]
+  },
+  "Greg" => {
+    :twitter => "techford",
+    :lottery_numbers => [32, 33, 34, 35, 36, 37],
+    :home_town => "Milton of Campsie",
+    :pets => [
+      {
+        :name => "tess",
+        :species => "dog"
+      }
+    ]
   }
 }
 
-# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# 2. Get Erik's hometown
-# 3. Get the array of Erik's lottery numbers
-# 4. Get the type of Avril's pet Monty
-# 5. Get the smallest of Erik's lottery numbers
-# 6. Return an array of Avril's lottery numbers that are even
-# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-# 8. Change Erik's hometown to Edinburgh
+# # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# p users["Jonathan"][:twitter]
+# # 2. Get Erik's hometown
+# p users["Erik"][:home_town]
+# # 3. Get the array of Erik's lottery numbers
+# p users["Erik"][:lottery_numbers]
+# # 4. Get the type of Avril's pet Monty - INCOMPLETE - accessing nested keys?
+# p users["Avril"][:pets][:species]
+# # 5. Get the smallest of Erik's lottery number
+# def smallest_number(smallest_number)
+#   smallest = 100
+#   for number in smallest_number
+#     if number < smallest
+#       smallest = number
+#     end
+#   end
+#   return smallest
+# end
+#
+# p smallest_number(users["Erik"][:lottery_numbers])
+# # 6. Return an array of Avril's lottery numbers that are even
+# def even_numbers(numbers)
+#   for number in numbers
+#       if number%2 == 0
+#          p number
+#       end
+#   end
+# end
+#
+# p even_numbers(users["Avril"][:lottery_numbers])
+# # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+# p users["Erik"][:lottery_numbers].push(7)
+# # 8. Change Erik's hometown to Edinburgh
+# users["Erik"][:home_town].delete!("Linlithgow")
+# users["Erik"][:home_town] = "Edinburgh"
+# p users["Erik"][:home_town]
+# # OR change the information in the hash directly
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
+# p users["Greg"][:lottery_numbers]
--- arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -1,3 +1,9 @@
+# Complete these tasks:
+# Change the capital of Wales from "Swansea" to "Cardiff".
+# Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+# Use a loop to print the names of all the countries in the UK.
+# Use a loop to find the total population of the UK.
+
 united_kingdom = [
   {
     name: "Scotland",
